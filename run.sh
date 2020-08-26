365a489e5aed46aa10c967bc5a711876bca8fbff
BeentageBand <josera.baez@gmail.com>
2020-08-26 08:00:37 -0700

    add links for github and linkedin

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 . common.sh
 
 docker run -v ${HOST_PATH}:${TARGET_PATH} -p ${HOST_PORT}:${TARGET_PORT} -e PORT=${TARGET_PORT} -w ${TARGET_PATH}/${PREFIX}/build -it ${PREFIX}-cc ./${PREFIX}
