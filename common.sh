6065787657ac0a115b05424c0a59390ba2a0aecc
BeentageBand <josera.baez@gmail.com>
2020-09-19 23:09:40 -0700

    Add MONGODB password and user configuration environment

--- common.sh
+++ common.sh
@@ -1,7 +1,10 @@
+MONGOUSER=${MONGODB_USER:?MONGODB_USER not defined}
+MONGOPASS=${MONGODB_USER:?MONGODB_PASS not defined}
 PREFIX=linktree
 HOST_PATH=$(realpath .)
 HOST_PORT=8080
 
 TARGET_PATH=/usr/src/linktree-cc
 TARGET_PORT=8080
-MONGODB_URI="mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@cluster0.lcfyt.mongodb.net/contactlist?retryWrites=true&w=majority"
+MONGODB_DB=contactlist
+MONGODB_URI="mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@cluster0.lcfyt.mongodb.net/${MONGODB_DB}?retryWrites=true&w=majority"
