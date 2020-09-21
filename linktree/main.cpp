d3077653f540c9f4e793b2bf4272fd5c98ce766e
BeentageBand <josera.baez@gmail.com>
2020-09-20 20:21:00 -0700

    Implement APIs contacts list and contact retrieve

--- linktree/main.cpp
+++ linktree/main.cpp
@@ -1,5 +1,5 @@
 #include "crow_all.h"
-#include "static_handler.h"
+#include "static-handler.h"
 #include <fstream>
 #include <iostream>
 #include <vector>
@@ -16,60 +16,82 @@
 #include <mongocxx/uri.hpp>
 #include <mongocxx/instance.hpp>
 
-int main (int argc, char ** argv) {
+std::string getView(std::string const &filename, crow::mustache::context &x)
+{
+  return crow::mustache::load("../public/" + filename + ".html").render(x);
+}
+
+int main(int argc, char **argv)
+{
   crow::SimpleApp app;
+  crow::mustache::set_base(".");
+
   mongocxx::instance inst{};
   std::string mongo_uri = std::string(getenv("MONGODB_URI"));
-  lk::StaticHandler static_handler;
 
   mongocxx::client conn{mongocxx::uri{mongo_uri}};
   auto collection = conn["contactlist"]["contacts"];
 
-  char * port = getenv("PORT");
-  uint16_t i_port = static_cast<uint16_t> (nullptr != port ? std::stoi(port): 18080);
+  char *port = getenv("PORT");
+  uint16_t i_port = static_cast<uint16_t>(nullptr != port ? std::stoi(port) : 18080);
   std::cout << "PORT : " << i_port << std::endl;
 
+  lk::StaticHandler static_handler;
+
   CROW_ROUTE(app, "/styles/<string>")
-  ([&static_handler] (crow::request const & req, crow::response & res, std::string filename) {
+  ([&static_handler](crow::request const &req, crow::response &res, std::string filename) {
     static_handler.send_style(res, std::move(filename));
   });
 
   CROW_ROUTE(app, "/images/<string>")
-  ([&static_handler] (crow::request const & req, crow::response & res, std::string filename) {
+  ([&static_handler](crow::request const &req, crow::response &res, std::string filename) {
     static_handler.send_image(res, std::move(filename));
   });
 
   CROW_ROUTE(app, "/scripts/<string>")
-  ([&static_handler] (crow::request const & req, crow::response & res, std::string filename) {
+  ([&static_handler](crow::request const &req, crow::response &res, std::string filename) {
     static_handler.send_script(res, std::move(filename));
   });
 
   CROW_ROUTE(app, "/about")
-  ([&static_handler] (crow::request const & req, crow::response & res) {
+  ([&static_handler](crow::request const &req, crow::response &res) {
     static_handler.send_html(res, "about");
   });
 
-  CROW_ROUTE(app, "/api/contacts") 
+  CROW_ROUTE(app, "/api/contacts/<string>")
+  ([&collection](std::string email) {
+    auto doc = collection.find_one(bsoncxx::builder::basic::make_document(
+      bsoncxx::builder::basic::kvp("email", email)));
+    crow::json::wvalue dto;
+
+    dto["contact"] = crow::json::load(bsoncxx::to_json(doc.value().view()));
+    return getView("contact", dto);
+  });
+
+  CROW_ROUTE(app, "/api/contacts")
   ([&collection](void) {
-     mongocxx::options::find opts;
-     opts.skip(9);
-     opts.limit(10);
-     auto docs = collection.find({}, opts);
-     std::ostringstream os;
-     for (auto && doc : docs) {
-        os << bsoncxx::to_json(doc) << "\n";
-     }
-     return crow::response(os.str());
-   });
+    mongocxx::options::find opts;
+    opts.skip(9);
+    opts.limit(10);
+    auto docs = collection.find({}, opts);
+    crow::json::wvalue dto;
+    std::vector<crow::json::rvalue> contacts;
+    for (auto doc : docs)
+    {
+      contacts.push_back(crow::json::load(bsoncxx::to_json(doc)));
+    }
+    dto["contacts"] = contacts;
+    return getView("contacts", dto);
+  });
 
   CROW_ROUTE(app, "/<string>")
-  ([&static_handler] (crow::request const & req, crow::response & res, std::string uri) {
+  ([&static_handler](crow::request const &req, crow::response &res, std::string uri) {
     res.redirect("/");
     res.end();
   });
 
   CROW_ROUTE(app, "/")
-  ([&static_handler] (crow::request const & req, crow::response & res) {
+  ([&static_handler](crow::request const &req, crow::response &res) {
     static_handler.send_html(res, "index");
   });
 
